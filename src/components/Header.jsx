5e8937c4c9f34bc3493e12138e0f756d847aff47
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-14 13:29:19 -0700

    header footer and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <img src="./images/ms-logo.svg" alt="mariners logo" />
+      <h1>Mariners</h1>
+    </header>
+  )
 }
 
 export default Header;
