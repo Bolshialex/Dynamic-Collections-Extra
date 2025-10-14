aab4a64defda1b015e178e6196efb453172f5c23
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-14 13:28:57 -0700

    Add player card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function PlayerCard() {
-  return <div></div>;
+function PlayerCard(players) {
+  return (
+    <>
+      <div className="player-card">
+        <img src={players.img} alt={players.name} />
+        <h3>{players.name}</h3>
+        <p className="position">Position {players.position}</p>
+        <p className="jersey">#{players.jersey}</p>
+        <p className="age">Age: {players.name}</p>
+        <p className="stats">Batting AVG: {players.stat}</p>
+      </div>
+    </>
+  );
 }
 
 export default PlayerCard;
